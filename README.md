359cde0cb0ce8e1d59812462e5a64c1b902a115a
Terry Rice <trice7454@gmail.com>
2025-06-24 19:01:24 -0500

    Menu and frame added and updated readme

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Speaking of Boot.dev, this project is my Boot.dev personal
 project. Talk about killing two birds with one stone. I get 
 to prototype the user interface in Python wxWidgets and complete
 a project.
+
+## Running it 
+To be able to run the project you either need to have wxPython
+installed or set up your venv using the following:
+
+```
+python3 -m venv .venv
+pip install wxPython
+
+python3 ddocdpy.py
+```
--- ddocdpy.py
+++ ddocdpy.py
@@ -1,6 +1,44 @@
 import wx
 
-app = wx.App()
-frm = wx.Frame(None, title="DDO CD (Prototype)")
-frm.Show()
-app.MainLoop()
+class DdoFrame(wx.Frame):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+
+        panel = wx.Panel(self)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        panel.SetSizer(sizer)
+
+        self.makeMenuBar()
+
+    def makeMenuBar(self):
+        fileMenu = wx.Menu()
+
+        newCharacterMenuItem = fileMenu.Append(-1, "&New Character...\tCtrl-N",
+                                               "Create a new character")
+
+        fileMenu.AppendSeparator()
+
+        exitMenuItem = fileMenu.Append(wx.ID_EXIT)
+
+        theMenu = wx.MenuBar()
+        theMenu.Append(fileMenu, "&File")
+
+        self.SetMenuBar(theMenu)
+
+        self.Bind(wx.EVT_MENU, self.onNewCharacter, newCharacterMenuItem)
+        self.Bind(wx.EVT_MENU, self.onExit, exitMenuItem)
+
+
+    def onNewCharacter(self, event):
+        wx.MessageBox("Coming soon...")
+
+    def onExit(self, event):
+        self.Close(True)
+
+if __name__ == '__main__':
+    app = wx.App()
+    frm = DdoFrame(None, title="DDO CD (Prototype)")
+    frm.Show()
+    app.MainLoop()
+
