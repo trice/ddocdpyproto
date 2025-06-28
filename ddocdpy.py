1e1c035cc0721ee290fa1de0dad786a133225110
Terry Rice <trice7454@gmail.com>
2025-06-27 21:26:30 -0500

    Updated UI with About and Exit etc.

--- ddocdpy.py
+++ ddocdpy.py
@@ -10,7 +10,26 @@ class DdoApplication(wx.App):
     def init_frame(self):
         self.window_frame = self.res.LoadFrame(None, 'DdoCdFrame')
         self.window_frame.Show()
+        self.Bind(wx.EVT_MENU, self.on_new_character, id = xrc.XRCID('new_character_menu'))
+        self.Bind(wx.EVT_MENU, self.on_save_character, id = xrc.XRCID('save_character_menu'))
+        self.Bind(wx.EVT_MENU, self.on_about, id = xrc.XRCID('about_menu_item'))
+        self.Bind(wx.EVT_MENU, self.on_exit, id=xrc.XRCID('exit_menu_item'))
+                  
 
+    def on_about(self, event):
+        # Load the dialog from XRC
+        about_dlg = self.res.LoadDialog(self.GetTopWindow(), "AboutDialog")
+        about_dlg.ShowModal()
+        about_dlg.Destroy()
+
+    def on_new_character(self, event):
+        wx.MessageBox("Coming Soon...")
+
+    def on_save_character(self, event):
+        wx.MessageBox("Saving Character...")
+
+    def on_exit(self, event):
+        self.window_frame.Close()
 
 if __name__ == '__main__':
     app = DdoApplication(False)
