bbe8ae901c8d944c0af9b7489acb7b8a82f8fc5b
Terry Rice <trice7454@gmail.com>
2025-07-04 10:20:44 -0500

    Fixed all the style issues

--- ddocdpy.py
+++ ddocdpy.py
@@ -1,6 +1,7 @@
 import wx
 from wx import xrc
 
+
 class DdoApplication(wx.App):
     def OnInit(self):
         self.res = xrc.XmlResource('MyProjectBase.xrc')
@@ -10,11 +11,12 @@ class DdoApplication(wx.App):
     def init_frame(self):
         self.window_frame = self.res.LoadFrame(None, 'DdoCdFrame')
         self.window_frame.Show()
-        self.Bind(wx.EVT_MENU, self.on_new_character, id = xrc.XRCID('new_character_menu'))
-        self.Bind(wx.EVT_MENU, self.on_save_character, id = xrc.XRCID('save_character_menu'))
-        self.Bind(wx.EVT_MENU, self.on_about, id = xrc.XRCID('about_menu_item'))
+        self.Bind(wx.EVT_MENU, self.on_new_character,
+                  id=xrc.XRCID('new_character_menu'))
+        self.Bind(wx.EVT_MENU, self.on_save_character,
+                  id=xrc.XRCID('save_character_menu'))
+        self.Bind(wx.EVT_MENU, self.on_about, id=xrc.XRCID('about_menu_item'))
         self.Bind(wx.EVT_MENU, self.on_exit, id=xrc.XRCID('exit_menu_item'))
-
 
     def on_about(self, event):
         # Load the dialog from XRC
@@ -31,7 +33,7 @@ class DdoApplication(wx.App):
     def on_exit(self, event):
         self.window_frame.Close()
 
+
 if __name__ == '__main__':
     app = DdoApplication(False)
     app.MainLoop()
-
